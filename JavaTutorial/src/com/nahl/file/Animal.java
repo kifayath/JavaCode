cd1f2ea71aa0994ad507c3076a74bf432e9bc5ff
kifayath <kifayath.khan504@gmail.com>
2012-09-20 15:58:45 +0530

    Animal class

---
+++ JavaTutorial/src/com/nahl/file/Animal.java
@@ -0,0 +1,13 @@
+package com.nahl.file;
+
+public class Animal {
+
+	public static void testClassMethod()
+	{
+		System.out.println("The Class" + "Method in Animal");
+	}
+	public void testInstanceMethod()
+	{
+		System.out.println("The Instance" + "Method in Animal");
+	}
+}
